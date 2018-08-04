a345955309577ea47f9f45598a95908041fc8821
tishayaem <kostiantynkalinin@gmail.com>
2018-08-04 12:32:35 +0100

    Created Event component

---
+++ src/Event.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default class Event extends React.Component {
+  render() {
+    return (
+      <div className="event">
+        Event #1
+      </div>
+    )
+  }
+}
