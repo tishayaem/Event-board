c74a48292bb8672593feff007b0a4ed1d8b8303f
tishayaem <kostiantynkalinin@gmail.com>
2018-08-05 22:28:25 +0100

    Board component

---
+++ src/Board.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return(
+
+    )
+  }
+
+}
